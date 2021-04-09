b1dc90b92b337b42e3a2c9442239d5ada27ccb2b
Hayden Anderson <Hayden.Anderson@rexelusa.com>
2021-04-09 01:12:56 -0400

    add authentication for login and logout

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -6,10 +6,11 @@ export const authStart = () => {
     type: actionTypes.AUTH_START
   };
 }
-export const authSuccess = (data) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: data
+    idToken: token,
+    userId
   }
 }
 
@@ -19,6 +20,23 @@ export const authFail = (error) => {
     error: error
   }
 }
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000)
+  }
+}
+
+
+
 export const auth = (email, password, isSignUp) => {
   return dispatch => {
     //auth user
@@ -34,14 +52,12 @@ export const auth = (email, password, isSignUp) => {
     }
     axios.post(url, authData)
       .then(response => {
-
         console.log(response);
-        dispatch(authSuccess(response.data));
-      }
-      )
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
+      })
       .catch(err => {
-        console.log(err);
-        dispatch(authFail(err));
+        dispatch(authFail(err.response.data.error));
       })
   }
 }
